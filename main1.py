8ed5f8f6f0d99c50ffe6558836a97c5b3e592418
Anatou <anatole@desnot.com>
2021-01-06 22:24:45 +0100

    PChacker

--- main1.py
+++ main1.py
@@ -1,6 +1,8 @@
 import pygame
 from pygame.locals import *
 
+files = {'C:':{'dossier1':{'fichier1':1, 'dossier2':{}},'dossier3':{'fichier2':2, 'dossier4':{'fichier3':3, 'fichier4':4}}}}
+
 def render(toBlit, firstPlan) :
 	"""Fonction qui affiche les images spécfiée dans la liste de tuple en param2 dans l'ordre croissant des indices de la liste, sauf l'image spécifiée dans le tuple en param1, qui sera affiché en premier plan"""
 	#Si l'image à mettre en 1er plan l'est déjà
@@ -67,24 +69,82 @@ def appli2(imag) :
 					appli = False
 
 	return imag, conti
-	
+
 def printLog(l, imag) :
-	l=l.split("\n")
-	ligne=150
+	ligne=160
 	render(imag, None)
 	for line in l:
-		screen.blit(terminal.render(line, False, (0, 0, 0)), (300,ligne))
-		ligne+=30
+		screen.blit(terminal.render(line, False, (0, 175, 0)), (250,ligne))
+		ligne+=20
 	pygame.display.flip()
-	
+
+def getDictKeys(dict) :
+	'''fonction qui retourne les clés d'un dictionnaire. Prend en paramètre le dictionnaire'''
+	k = []
+	for key in dict.keys() :
+		k.append(key)
+	return k
+
+def convertPath(p) :
+	'''fonction qui transforme le path string spécifié en path list utilisable par les autres fonctions. Prend en param le path string'''
+	p = p.split('/')
+	if p[len(p)-1] == '' : #Dans le cas où path = "C:/"
+		#supprimer la dernière valeur de la liste p (car vide et gène pour le len(p) plus tard)
+		del p[len(p)-1]
+	return p
+
+def goto(p) :
+	'''fonction qui retourne le dictionnaire au bout du chemin spécifié. Prend en param le chemin qui mène au dictionnaire'''
+	#prépare le path pour la navigation à travers le dictionnaire
+	p = convertPath(p)
+	#Va jusqu'au chemin spécifié en redéfinissant plusieurs fois current pour être le dictionnaire de fin demandé
+	current = files[p[0]]
+	for i in range(len(p)-1):
+		current = current[p[i+1]]
+	return current
+
+def cd(p, target) :
+	'''fonction qui simule la commande qui accède à un dossier en mettant à jour la variable chemin. Prend en param1 le chemin d'origine et en param2 le dossier à entrer'''
+	if target == '..' : #Remonter d'un dossier
+		p = p[:p.rfind('/')]
+		if p  == 'C:' : #Si déjà au minimum alors
+			p += '/' #Réajoute le '/' de fin uniquement présent au dossier racine de l'arbre
+		return p
+	else : #Avancer d'un dossier
+		exist = False
+		for key in goto(p).keys() : #Test si dossier cible existe
+			if key == target :
+				exist = True
+		if not exist :
+			return p
+		if p  == 'C:/' : #Si au minimum alors
+			p = p[:len(p)-1] #retire le '/' de fin uniquement présent au dossier racine de l'arbre
+		p = p+'/'+target
+		return p
+
+def ls(p) :
+	'''fonction qui simule la commande qui liste les fichiers et dossier présents dans le dossier où l'on se trouve. Prend en param le chemin actuel'''
+	#prépare le path pour la navigation à travers le dictionnaire
+	p = convertPath(p)
+	#Va jusqu'au chemin spécifié en redéfinissant plusieurs fois current pour être le dictionnaire de fin demandé
+	current = files[p[0]]
+	for i in range(len(p)-1):
+		current = current[p[i+1]]
+	#Affiche les clés présentes dans le chemin demandé
+	keys = getDictKeys(current)
+	return keys
+
 def PChacker(imag) :
+	path = 'C:/'
 	appli = True
 	conti = True
 	text=""
-	log=""
+	log=[]
 	input=None
 	output=None
-	ligne=150
+	ligne=160
+	screen.blit(terminal.render(path+" > ", False, (0, 175, 0)), (250,ligne))
+	pygame.display.flip()
 	while appli :
 		for event in pygame.event.get(): #Attente des événements
 			if event.type == QUIT:
@@ -100,30 +160,50 @@ def PChacker(imag) :
 			elif event.type == KEYDOWN :
 				if event.key == K_RETURN:
 					input = text
-					log+="> "+text+"\n"
+					log.append(path+" > "+text)
 					printLog(log, imag)
 					text = ''
-					ligne+=30
+					ligne+=20
 				elif event.key == K_BACKSPACE:
 					text = text[:-1]
 					printLog(log, imag)
-					screen.blit(terminal.render("> ", False, (0, 0, 0)), (300,ligne))
-					screen.blit(terminal.render(text, False, (0, 0, 0)), (320,ligne))
+					screen.blit(terminal.render(path+" > "+text, False, (0, 175, 0)), (250,ligne))
 					pygame.display.flip()
 				else:
 					text += event.unicode
-					screen.blit(terminal.render("> ", False, (0, 0, 0)), (300,ligne))
-					screen.blit(terminal.render(text, False, (0, 0, 0)), (320,ligne))
+					printLog(log, imag)
+					screen.blit(terminal.render(path+" > "+text, False, (0, 175, 0)), (250,ligne))
 					pygame.display.flip()
 		if input != None :
-			if input=="test":
+			input = input.split(" ")
+			if input[0]=="test":
 				output="1, 2, test !"
-				input = None
+			elif input[0] == 'ls' :
+				outp = ls(path)
+				output=""
+				for key in outp :
+					log.append(key)
+					ligne+=20
+				output=""
+			elif input[0] == 'cd' and len(input)>1 :
+				path = cd(path, input[1])
+			screen.blit(terminal.render(path+" > ", False, (0, 175, 0)), (250,ligne))
+			pygame.display.flip()
+			input = None
 		if output != None :
-			log+=output+"\n"
+			log.append(output)
 			printLog(log, imag)
-			ligne+=30
+			ligne+=20
 			output=None
+			screen.blit(terminal.render(path+" > ", False, (0, 175, 0)), (250,ligne))
+			pygame.display.flip()
+		if len(log) > 17 :
+			while len(log) > 17 :
+				del log[0]
+				ligne -=20
+			printLog(log, imag)
+			screen.blit(terminal.render(path+" > ", False, (0, 175, 0)), (250,ligne))
+			pygame.display.flip()
 
 	return imag, conti
 
@@ -132,8 +212,8 @@ pygame.init()
 pygame.font.init()
 
 
-myfont = pygame.font.SysFont('Arial', 30)
-terminal = pygame.font.Font('img/SLC_.ttf', 30)
+myfont = pygame.font.SysFont('Arial', 20)
+terminal = pygame.font.Font('img/SLC_.ttf', 20)
 text1 = myfont.render("I'm moving", False, (0, 0, 0))
 text2 = myfont.render("Je suis généré dynamiquement quand cette fenêtre est ouverte", False, (0,0,0))
 
@@ -147,7 +227,7 @@ screen.blit(fond, (0,0))
 
 #Icone de test 1
 icon = pygame.image.load("img/icon.png").convert()
-iconCoords = (100,639)
+iconCoords = (100,640)
 iconDim = icon.get_size()
 screen.blit(icon, iconCoords)
 
